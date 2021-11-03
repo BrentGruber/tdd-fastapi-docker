b822e3561701c067a68e7e5e2024e35675c45598
Brent <bg240711@outlook.com>
2021-11-02 20:54:00 -0400

    black reformatting

--- project/tests/test_summaries.py
+++ project/tests/test_summaries.py
@@ -57,7 +57,7 @@ def test_read_summary_incorrect_id(test_app_with_db):
                 "loc": ["path", "id"],
                 "msg": "ensure this value is greater than 0",
                 "type": "value_error.number.not_gt",
-                "ctx": {"limit_value": 0}
+                "ctx": {"limit_value": 0},
             }
         ]
     }
@@ -100,7 +100,7 @@ def test_remove_summary_incorrect_id(test_app_with_db):
                 "loc": ["path", "id"],
                 "msg": "ensure this value is greater than 0",
                 "type": "value_error.number.not_gt",
-                "ctx": {"limit_value": 0}
+                "ctx": {"limit_value": 0},
             }
         ]
     }
@@ -114,7 +114,7 @@ def test_update_summary(test_app_with_db):
 
     response = test_app_with_db.put(
         f"/summaries/{summary_id}/",
-        data=json.dumps({"url": "https://foo.bar", "summary": "updated!"})
+        data=json.dumps({"url": "https://foo.bar", "summary": "updated!"}),
     )
     assert response.status_code == 200
 
@@ -128,14 +128,14 @@ def test_update_summary(test_app_with_db):
 def test_update_summary_incorrect_id(test_app_with_db):
     response = test_app_with_db.put(
         "/summaries/999/",
-        data=json.dumps({"url": "https://foo.bar", "summary": "updated!"})
+        data=json.dumps({"url": "https://foo.bar", "summary": "updated!"}),
     )
     assert response.status_code == 404
     assert response.json()["detail"] == "Summary not found"
 
     response = test_app_with_db.put(
         "/summaries/0/",
-        data=json.dumps({"url": "https://foo.bar", "summary": "updated!"})
+        data=json.dumps({"url": "https://foo.bar", "summary": "updated!"}),
     )
     assert response.status_code == 422
     assert response.json() == {
@@ -144,7 +144,7 @@ def test_update_summary_incorrect_id(test_app_with_db):
                 "loc": ["path", "id"],
                 "msg": "ensure this value is greater than 0",
                 "type": "value_error.number.not_gt",
-                "ctx": {"limit_value": 0}
+                "ctx": {"limit_value": 0},
             }
         ]
     }
@@ -156,10 +156,7 @@ def test_update_summary_invalid_json(test_app_with_db):
     )
     summary_id = response.json()["id"]
 
-    response = test_app_with_db.put(
-        f"/summaries/{summary_id}/",
-        data=json.dumps({})
-    )
+    response = test_app_with_db.put(f"/summaries/{summary_id}/", data=json.dumps({}))
     assert response.status_code == 422
     assert response.json() == {
         "detail": [
@@ -171,8 +168,8 @@ def test_update_summary_invalid_json(test_app_with_db):
             {
                 "loc": ["body", "summary"],
                 "msg": "field required",
-                "type": "value_error.missing"
-            }
+                "type": "value_error.missing",
+            },
         ]
     }
 
@@ -184,8 +181,7 @@ def test_update_summary_invalid_keys(test_app_with_db):
     summary_id = response.json()["id"]
 
     response = test_app_with_db.put(
-        f"/summaries/{summary_id}/",
-        data=json.dumps({"url": "https://foo.bar"})
+        f"/summaries/{summary_id}/", data=json.dumps({"url": "https://foo.bar"})
     )
     assert response.status_code == 422
     assert response.json() == {
@@ -200,7 +196,7 @@ def test_update_summary_invalid_keys(test_app_with_db):
 
     response = test_app_with_db.put(
         f"/summaries/{summary_id}/",
-        data=json.dumps({"url": "invalid://url", "summary": "updated!"})
+        data=json.dumps({"url": "invalid://url", "summary": "updated!"}),
     )
     assert response.status_code == 422
     assert response.json()["detail"][0]["msg"] == "URL scheme not permitted"
